24f17ba181029f05d7b221b5f719a67e45fc36ae
eckama11 <eckhardt.aman@uwlax.edu>
2014-03-10 12:35:32 -0500

    Imported a Green bootstrap.css file
    Changed color of bootstrap to make more unique.

--- Employee/MyInfo.php
+++ Employee/MyInfo.php
@@ -4,7 +4,7 @@
  		<title>Employee</title>
  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- StyleSheet -->
-		<link rel="stylesheet" href="../css/bootstrap.css" />
+		<link rel="stylesheet" href="../css/bootstrap.min.css" />
 		<link rel="stylesheet" href="../css/custom.css" />
 	</head>
  
