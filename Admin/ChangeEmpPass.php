5e4c84ec6308e3bf47dde6ea9d2fe2473d1b9225
eckama11 <eckhardt.aman@uwlax.edu>
2014-03-13 18:06:56 -0500

    Add pages for Emp and Admin
    Shell pages for all use cases. May need common php code files to
    integrate all pages.

---
+++ Admin/ChangeEmpPass.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+ 		<title>Change Employee Password</title>
+ 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- StyleSheet -->
+		<link rel="stylesheet" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" href="../css/custom.css" />
+	</head>
+ 
+	<body>
+		<div class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<a href="#" class="navbar-brand">UPay Solutions</a>
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<div class="collapse navbar-collapse navHeaderCollapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="MyPay.php">MyPay</a></li>
+						<li><a href="MyInfo.php">MyInfo</a></li>
+						<li><a href="Pass.php">Account Settings</a></li>
+						<li><a href="Admin.php">Admin</a></li>
+						<li><a href="logout.php">Logout</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div>
+		</div>
+	</body>
+</html>
