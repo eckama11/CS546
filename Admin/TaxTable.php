d7ad76b404daffc20a864de7e7da345518b72fa2
Brad Fisher <fisher.brad@uwlax.edu>
2014-03-24 19:08:40 -0500

    Added more details to the tax table info page to describe the tax computation process and standard/claimed deduction amounts.

--- Admin/TaxTable.php
+++ Admin/TaxTable.php
@@ -16,6 +16,16 @@
 ?>
 <div class="container col-md-6 col-md-offset-3">
     <legend>Defined Tax Rates</legend>
+    <table class="table">
+    <tr>
+        <th>Standard Deduction</th>
+        <td style="text-align:right">$ 5,000.00</td>
+    </tr>
+    <tr>
+        <th>Per Deduction Allowance</th>
+        <td style="text-align:right">$ 1,000.00</td>
+    </tr>
+    </table>
     <table class="table table-striped table-bordered table-condensed">
     <thead><tr>
       <th>ID</th>
@@ -33,5 +43,46 @@
     } 
 ?>
     </tbody>
+    </table>
+    
+    <p>For example, an employee who is paid a yearly salary of $35,000 and claims 2 deductions would be taxed as follows:</p>
+    
+    <table class="table">
+    <tr>
+        <th>1) Gross Income</th>
+        <td style="text-align:right">$ 35,000.00</td>
+    </tr>
+    <tr>
+        <th>2) Standard Deduction</th>
+        <td style="text-align:right">$ 5,000.00</td>
+    </tr>
+    <tr>
+        <th>3) Deductions Claimed</th>
+        <td style="text-align:right">2</td>
+    </tr>
+    <tr>
+        <th>4) Deduction Allowance<br/>($1,000.00 times line 3)</th>
+        <td style="text-align:right">$ 2,000.00</td>
+    </tr>
+    <tr>
+        <th>5) Total Deductions<br/>(line 2 plus line 4)</th>
+        <td style="text-align:right">$ 7,000.00</td>
+    </tr>
+    <tr>
+        <th>5) Taxable Income<br/>(line 1 minus line 5)</th>
+        <td style="text-align:right">$ 28,000.00</td>
+    </tr>
+    <tr>
+        <th>6) Tax Rate Applied<br/>(from tax table above)</th>
+        <td style="text-align:right">15.00 %</td>
+    </tr>
+    <tr>
+        <th>7) Tax<br/>(line 5 times line 6)</th>
+        <td style="text-align:right">$ 4,200.00</td>
+    </tr>
+    <tr>
+        <th>8) After Tax Income<br/>(line 1 minus line 7)</th>
+        <td style="text-align:right">$ 30,800.00</td>
+    </tr>
     </table>
 </div>
