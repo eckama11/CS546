a8af3a4685576f68734d4f4c5fc36c9f965cf69e
eckama11 <eckhardt.aman@uwlax.edu>
2014-04-28 19:09:11 -0500

    Create ProjectInfo.php
    - created ProjectInfo.php
    - currently finds other costs and name of project
    - needs to make array of employee and department with cost associated
    with employee and department

---
+++ Admin/ProjectInfo.php
@@ -0,0 +1,55 @@
+<?php
+    require_once(dirname(__FILE__)."/../common.php");
+    if (!isset($loginSession))
+        doUnauthenticatedRedirect();
+
+    if (!$loginSession->isAdministrator)
+        doUnauthorizedRedirect();
+
+    $projectId = @$_GET['id'];
+    $project = null;
+
+//An administrator should be able to generate a report on each project which should display the people associated with the project and the total cost.
+
+?>
+<!--Load the AJAX API-->
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script type="text/javascript">
+
+  // Load the Visualization API and the piechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+  function drawChart() {
+
+	// Create the data table.
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Costs');
+	data.addColumn('number', 'Dollars');
+	data.addRows([
+	  ['Other Costs', <?= json_encode($db->readProject($projectId)->otherCosts) ?>],
+	  ['Onions', 1],
+	  ['Olives', 1],
+	  ['Zucchini', 1],
+	  ['Pepperoni', 2]
+	]);
+
+	// Set chart options
+	var options = {'title':<?= json_encode($db->readProject($projectId)->name) ?>,
+				   'width':800,
+				   'height':500};
+
+	// Instantiate and draw our chart, passing in some options.
+	var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+	chart.draw(data, options);
+  }
+</script>
+
+<div class="container col-md-6 col-md-offset-3">
+	<div id="chart_div"></div>
+<div>
